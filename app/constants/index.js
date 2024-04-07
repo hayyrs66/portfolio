ba2ffa07777b9b951d39fd09e9e9978b342144f0
hayyrs66 <rayrtsx@proton.me>
2024-04-07 12:27:17 -0600

    Update works in app/constants/index.js

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,6 +1,6 @@
 export const works = [
   {
-    title: "Fashion E-Commerce",
+    title: "Fashion",
     category: "Web Development",
     year: "2023",
     description:
@@ -9,6 +9,39 @@ export const works = [
     src: ["/media/hero.png", "/media/hero2.png"],
     stack: ["Nextjs ", "React ", "TypeScript", "PayloadCMS", "Stripe"],
     number: "01",
+  },
+  {
+    title: "Footprint",
+    category: "Web Development",
+    year: "2024",
+    description:
+      "Web application to calculate the ecological footprint of Google services using the Drive API v3, employing conversion factors to raise awareness about a healthier and renewable digital life.",
+    href: "https://github.com/hayyrs66/footprint",
+    src: ["/media/footprint.png", "/media/footprint-2.png"],
+    stack: ["Astro ", "Expressjs"],
+    number: "02",
+  },
+  {
+    title: "Encryptor",
+    category: "Web Development",
+    year: "2024",
+    description:
+      "This is a simple web built with Astro that allows users to encrypt and decrypt text. Users can input text, choose to encrypt or decrypt it, and copy the resulting text to the clipboard.",
+    href: "https://encryptor-dusky.vercel.app/",
+    src: ["/media/encryptor.png", "/media/encryptor-2.png"],
+    stack: ["Astro ", "TailwindCSS"],
+    number: "03",
+  },
+  {
+    title: "Currency",
+    category: "Backend",
+    year: "2024",
+    description:
+      "This is a Java console application for currency conversion. It allows users to convert between different currencies using real-time exchange rates obtained from the ExchangeRate-API.",
+    href: "https://github.com/hayyrs66/currency-converter",
+    src: ["/media/currency.png", "/media/currency-2.png"],
+    stack: ["Astro ", "TailwindCSS"],
+    number: "03",
   },
 ];
 
