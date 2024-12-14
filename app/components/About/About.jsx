ca50980ff2af135f74179ccac9e7467b5d828034
ricardo <rayrtsx@proton.me>
2024-12-13 23:00:32 -0600

    Update About component: change image format and improve text formatting

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
   const controls = useAnimation();
 
   const [ref, inView] = useInView({
-    triggerOnce: true
+    triggerOnce: true,
   });
 
   const handleScroll = () => {
@@ -32,7 +32,7 @@ export default function About() {
         <div className={styles.grid}>
           <motion.picture animate={controls} initial={{ y: -50 }}>
             <Image
-              src={"/media/ricardo_3.JPG"}
+              src={"/media/ricardo.avif"}
               fill
               alt="Ricardo Guevara"
               title="Ricardo Guevara"
@@ -41,11 +41,27 @@ export default function About() {
           </motion.picture>
           <div>
             <p>
-              With a high school diploma in Computer Science and currently in my second year of the Software Engineering program at <a href="https://principal.url.edu.gt/" target="_blank">Rafael Landivar University <CgExternal /></a>, I've embarked on a journey that highlights my dedication to both structured and self-directed learning. Engaging in hands-on projects, I continuously broaden my understanding of the field, laying a strong foundation for my future career in software engineering.
+              With a high school diploma in Computer Science and currently in my
+              third year of the Software Engineering program at{" "}
+              <a href="https://principal.url.edu.gt/" target="_blank">
+                Rafael Landivar University <CgExternal />
+              </a>
+              I've embarked on a journey that highlights my dedication to both
+              structured and self-directed learning. Engaging in hands-on
+              projects, I continuously broaden my understanding of the field,
+              laying a strong foundation for my future career in software
+              engineering.
             </p>
             <br />
             <p>
-              My approach to learning and development is deeply influenced by professionals like Daniel Meja, a Software Lead Engineer at Telus International. His guidance has shaped my understanding of clean code practices, personal growth, and the pursuit of excellence in software engineering. Additionally, the insights I've gained from Midudev have reinforced the importance of consistency in web technologies, adherence to best practices, and attention to detail in the development process.
+              My approach to learning and development is deeply influenced by
+              professionals like Daniel Meja, a Software Lead Engineer at Telus
+              International. His guidance has shaped my understanding of clean
+              code practices, personal growth, and the pursuit of excellence in
+              software engineering. Additionally, the insights I've gained from
+              Midudev have reinforced the importance of consistency in web
+              technologies, adherence to best practices, and attention to detail
+              in the development process.
             </p>
             <br />
           </div>
